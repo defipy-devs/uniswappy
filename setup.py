f20b4dc9603954073f16f0c2600b847825b64d4b
icmoore <ic3moore@gmail.com>
2023-09-30 18:35:13 -0700

    update README

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(name='UniswapPy',
-      version='1.0.1',
+      version='1.0.3',
       description='Uniswap for Python',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/icmoore/uniswappy',
       author = "icmoore",
       author_email = "imoore@syscoin.org",

2e820852e199e1fcca3f592c6f16d52b61db5ab8
icmoore <ic3moore@gmail.com>
2024-01-25 20:53:29 -0800

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='UniswapPy',
-      version='1.1.1',
+      version='1.1.2',
       description='Uniswap for Python',
       long_description=long_description,
       long_description_content_type="text/markdown",

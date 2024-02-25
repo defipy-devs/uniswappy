a6f847fde96623677024d5eddb22dfb38c7e7964
BrotherBaner <25873736+ABusyHippie@users.noreply.github.com>
2024-02-25 00:14:22 -0700

    live chart data add

---
+++ notebooks/research/bokeh_server.py
@@ -0,0 +1,55 @@
+from bokeh.plotting import figure, curdoc
+from bokeh.models import ColumnDataSource, CustomJS, Range1d, NumeralTickFormatter
+from uniswappy.utils.client import API0x
+import time
+from bokeh.layouts import layout
+
+# Initialize Bokeh figure and data source
+p = figure(title='USDC to WETH Price', x_axis_label='Time', y_axis_label='Price', width=800, height=400)
+source = ColumnDataSource(data={'x': [], 'y': []})
+p.line(x='x', y='y', source=source)
+
+timestamp_counter = 0
+timestamp = time.time()
+
+def update_data():
+    global timestamp_counter
+    api = API0x()
+    sell_token = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'  # USDC
+    buy_token = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'  # WETH
+    sell_amount = 100000000  # 100 USDC (USDC has a base unit of 6)
+    data_json = api.apply(sell_token, buy_token, sell_amount)
+    
+    price = data_json['price']
+    price_numeric = float(price)
+    
+    # Update data source
+    new_data = {'x': [timestamp_counter], 'y': [price_numeric]}
+    source.stream(new_data, rollover=100)  # Adjust rollover as needed
+
+    timestamp_counter += 30
+
+    current_time = time.time() - timestamp
+
+    print("Price: ", price_numeric)
+    # print("Time: ", current_time)
+
+# Add periodic callback to update data every 30 seconds
+curdoc().add_periodic_callback(update_data, 30000)
+
+def adjust_y_range():
+    if len(source.data['y']) > 0:
+        min_price = min(source.data['y'])
+        max_price = max(source.data['y'])
+        y_range_min = min_price - 0.1 * min_price
+        y_range_max = max_price + 0.1 * max_price
+        p.y_range = Range1d(start=y_range_min, end=y_range_max)
+        p.yaxis.formatter = NumeralTickFormatter(format='0,0.000000')
+
+curdoc().add_periodic_callback(adjust_y_range, 1000)
+
+# Add plot to the document
+layout = layout([[p]], sizing_mode='scale_width')
+
+# Add layout to the document
+curdoc().add_root(layout)

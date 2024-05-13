536dbbc75f80e5efacbe39e73a2889c8e4bb34a4
icmoore <ic3moore@gmail.com>
2024-05-13 15:58:00 -0700

    RebaseIndexToken bug

--- python/prod/cpt/index/RebaseIndexToken.py
+++ python/prod/cpt/index/RebaseIndexToken.py
@@ -61,26 +61,27 @@ class RebaseIndexToken():
     def calc_univ3_tkn_settlement(self, lp, token_in, dL, lwr_tick, upr_tick):
                 
         L = lp.get_liquidity()
+        L_diff = (L - dL) 
         if(token_in.token_name == lp.token0):
-            sqrtp_cur = lp.slot0.sqrtPriceX96/2**96
             sqrtp_pa = TickMath.getSqrtRatioAtTick(lwr_tick)/2**96
             sqrtp_pb = TickMath.getSqrtRatioAtTick(upr_tick)/2**96 
+            sqrtp_cur = lp.slot0.sqrtPriceX96/2**96 
             dPy = (sqrtp_cur - sqrtp_pa)
-            dPx = (1/sqrtp_cur - 1/sqrtp_pb)          
+            dPx = (1/sqrtp_cur - 1/sqrtp_pb)  
+            dx = dL*dPx
+            dy = dL*dPy    
+            sqrtp_next = sqrtp_cur + (997*dy)/(L_diff*1000) 
+            itkn_amt = dx + L_diff * (1/sqrtp_cur - 1/sqrtp_next)
         elif(token_in.token_name == lp.token1):
             sqrtp_cur = 2**96/lp.slot0.sqrtPriceX96
             sqrtp_pa = 2**96/TickMath.getSqrtRatioAtTick(lwr_tick)
             sqrtp_pb = 2**96/TickMath.getSqrtRatioAtTick(upr_tick)
-            dPx = (1/sqrtp_cur - 1/sqrtp_pa)
-            dPy = (sqrtp_cur - sqrtp_pb)
-    
-        dAmt = dL*dPx
-        
-        L_diff = (L - dL) 
-        sqrtp_next = sqrtp_cur + (997*dAmt)/(L_diff*1000) 
-        dy1 = dAmt
-        dy2 = L_diff * (1/sqrtp_cur - 1/sqrtp_next)
-        itkn_amt = dy1 + dy2
+            dPy = (1/sqrtp_cur - 1/sqrtp_pa)
+            dPx = (sqrtp_cur - sqrtp_pb)
+            dx = dL*dPx
+            dy = dL*dPy
+            sqrtp_next = sqrtp_cur + (997*dx)/(L_diff*1000) 
+            itkn_amt = dy + L_diff * (1/sqrtp_cur - 1/sqrtp_next)
 
         return itkn_amt if itkn_amt > 0 else 0 
     
